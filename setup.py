6065fb49d720a2cf4c51080a82d70440ae0afa69
Harshal Kumre <kumreharshalkumar@gmail.com>
2023-02-19 00:02:52 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from typing import List
+
+
+# Declaring variables for setup functions
+PROJECT_NAME="insurance"
+VERSION="0.0.3"
+AUTHOR="Harshal Kumre"
+DESCRIPTION="This is a machine learning regression project to predict the medical insurance premium."
+REQUIREMENT_FILE_NAME="requirements.txt"
+
+def get_requirements_list() -> List[str]:
+    """
+    Description: This function is going to return list of requirement
+    mention in requirements.txt file.
+    
+    return This function is going to return a list which contain name 
+    of libraries mentioned in requirements.txt file. 
+    """
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        return requirement_file.readlines().remove("-e .")
+
+
+setup(
+    name=PROJECT_NAME,
+    version=VERSION,
+    author=AUTHOR,
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=get_requirements_list()
+)
